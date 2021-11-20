66fa33fa118021787aced36cf7b21c64ddb58be3
Parth <partharora99160808@gmail.com>
2021-11-21 03:06:17 +0530

    Make internal_info pointer constant

--- ImageInfo.hpp
+++ ImageInfo.hpp
@@ -30,7 +30,7 @@ namespace CIL {
                        << "\nColor space:\t" << static_cast<int>(color_model)
                        << "\n";
         }
-        void *internal_info = nullptr;
+        const void *internal_info = nullptr;
     };
 
 } // namespace CIL

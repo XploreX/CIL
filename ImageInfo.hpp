1c58d762da5f36440411ba6252437658c747c85f
Manorit <manorit2001@gmail.com>
2021-11-20 18:30:57 +0530

    update ImageInfo.hpp

--- ImageInfo.hpp
+++ ImageInfo.hpp
@@ -1,26 +1,34 @@
+#ifndef CIL_IMAGE_INFO_H
+#define CIL_IMAGE_INFO_H
 #include <iostream>
 #include <setjmp.h>
 #include <stdint.h>
 #include <stdio.h>
 
-typedef enum
-{
-    COLOR_RBG,
-    COLOR_RBGA,
-    COLOR_CMYK,
-} COLOR_MODELS;
-
-struct ImageInfo
-{
-    uint32_t width;
-    uint32_t height;
-    int num_components;
-    COLOR_MODELS color_model;
-    uint8_t* data;
-    void print_image_info()
+namespace CIL {
+    enum class COLOR_MODELS
     {
-        std ::cerr << "Width:\t" << width << "\nHeight:\t" << height
-                   << "\nNum Components:\t" << num_components
-                   << "\nColor space:\t" << color_model << "\n";
-    }
-};
+        COLOR_RBG,
+        COLOR_RBGA,
+        COLOR_CMYK,
+        COLOR_GRAYSCALE,
+    };
+
+    struct ImageInfo
+    {
+        uint32_t width;
+        uint32_t height;
+        int num_components;
+        COLOR_MODELS color_model;
+        uint8_t* data;
+        void print_image_info()
+        {
+            std ::cerr << "Width:\t" << width << "\nHeight:\t" << height
+                       << "\nNum Components:\t" << num_components
+                       << "\nColor space:\t" << static_cast<int>(color_model)
+                       << "\n";
+        }
+    };
+
+} // namespace CIL
+#endif
